137dbf38ae76ee6f718c5fee420641d184672f68
polarfox42 <vg.mkrtchyan@gmail.com>
2019-07-22 21:21:48 +0500

    update script

--- findtag.py
+++ findtag.py
@@ -1,72 +1,65 @@
 #!/usr/bin/env python3
 import os
 import sys
-from subprocess import call as start_file
 
 
-# TODO 1. input_tag_and_directory( принимает тег для поиска, директорию, где искать)
-def input_tag_and_directory():
+def get_substring_and_directory():
     # arguments_for_search = sys.argv
     if len(sys.argv) < 2:
-        print('Использование для Linux: python3 findtag.py [имя тега] [адрес директории поиска, '
-              'по умолчанию ищет в текущей]' + '\n' +
-              'Использование для Windows: py findtag.py [имя тега] '
-              '[адрес директории поиска, по умолчанию ищет в текущей]')
+        print('Использование: findtag.py [слово для поиска] [адрес директории поиска, '
+              'по умолчанию ищет в текущей] [адрес сохранения файла, по умолчанию сохраняет в директории поиска]')
         sys.exit()
+
     elif len(sys.argv) < 3:
-        print('Для поиска будет использована текущая директория')
-        return sys.argv[1], os.getcwd()
+        print('Для поиска и сохранения будет использована текущая директория.')
+        return sys.argv[1], os.getcwd(), os.getcwd()
 
-    return sys.argv[1], sys.argv[2]
+    elif len(sys.argv) < 4:
+        print('Для сохранения будет использована директория поиска.')
+        return sys.argv[1], sys.argv[2], sys.argv[2]
+
+    return sys.argv[1], sys.argv[2], sys.argv[3]
 
 
-# TODO 2. search_files_tag(принимает тег для поиска и адрес директории, в которой нужно искать) на данный момент ищет
-# только в указанной директории, без поддиректорий.
-def search_files(directory_search_tag, directory_path):
+def search_files(substring_for_search, directory_path):
     file_list = os.listdir(directory_path)
     result_file_list = []
     files = dict()
 
     for file in file_list:
         file_path = os.path.join(directory_path, file)
-        if os.path.isfile(file_path) and directory_search_tag in file:
+        if os.path.isfile(file_path) and substring_for_search in file:
             result_file_list.append(file)
-            files[file] = file_path
+            files[file] = ['file', file_path]
+        elif os.path.isdir(file_path) and substring_for_search in file:
+            result_file_list.append(file)
+            files[file] = ('dir ', file_path)
 
-    print('Найдены следующие файлы:')
+    if len(result_file_list) == 0:
+        return None, None
+
+    print('Найдены следующие соответствия:')
     result_list = sorted(result_file_list)
     for i in range(len(result_list)):
         print(f'{i}. {result_list[i]}')
     return result_list, files
 
 
-# TODO 3. file_select
-def file_select(result_list, files):
-    if len(result_list) > 0:
-        file_number = int(input('Выберите номер файла для открытия: '))
-        file = files[result_list[file_number]]
-
-        search_tag = input('Введите тег для поиска: ')
-
-        print(file, search_tag) # отладочная печать
-        return file, search_tag
-
-    print('Ничего не найдено.')
-    return None, None
-
-# TODO 4. open_in_file
-def open_file(file, search_tag) -> 'str':
-    arg_for_opened = "xdg-open " + file
-    start_file([arg_for_opened], shell=True)
-    print("Открыт файл с тегом ", search_tag)
-
+def create_file(result_list, files, directory_to_save):
+    file_to_save = os.path.join(directory_to_save, 'files_found.txt')
+    with open(file_to_save, 'w', encoding='utf-8') as save:
+        for i in range(len(result_list)):
+            save.write(f'{i}. {files[result_list[i]][0]} {files[result_list[i]][1]}\n')
+    print('Операция завершена.')
 
 
 def main() -> None:
-    directory_search_tag, directory_path = input_tag_and_directory()
-    result_list, files = search_files(directory_search_tag, directory_path)
-    file, search_tag = file_select(result_list, files)
-    open_file(file, search_tag)
+    substring_for_search, directory_path, directory_to_save = get_substring_and_directory()
+    result_list, files = search_files(substring_for_search, directory_path)
+    if result_list is None:
+        print('Ничего не найдено.')
+    else:
+        create_file(result_list, files, directory_to_save)
 
 
 if __name__ == "__main__":
