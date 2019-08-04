7e8c9a3e5527dc959cc13055b7c2e53c20bc9b61
polarfox42 <vg.mkrtchyan@gmail.com>
2019-08-04 15:50:37 +0500

    update search

--- findtag.py
+++ findtag.py
@@ -4,10 +4,11 @@ import sys
 
 
 def get_substring_and_directory():
-    # arguments_for_search = sys.argv
+    '''Recieve and return substring, path to search and path to save.'''
     if len(sys.argv) < 2:
         print('Использование: findtag.py [слово для поиска] [адрес директории поиска, '
-              'по умолчанию ищет в текущей] [адрес сохранения файла, по умолчанию сохраняет в директории поиска]')
+              'по умолчанию ищет в текущей] '
+              '[адрес сохранения файла, по умолчанию сохраняет в директории поиска]')
         sys.exit()
 
     elif len(sys.argv) < 3:
@@ -22,34 +23,38 @@ def get_substring_and_directory():
 
 
 def search_files(substring_for_search, directory_path):
+    '''Search files with substring in file names'''
     file_list = os.listdir(directory_path)
     result_file_list = []
-    files = dict()
+    files = {}
 
     for file in file_list:
         file_path = os.path.join(directory_path, file)
         if os.path.isfile(file_path) and substring_for_search in file:
             result_file_list.append(file)
-            files[file] = ['file', file_path]
-        elif os.path.isdir(file_path) and substring_for_search in file:
-            result_file_list.append(file)
-            files[file] = ('dir ', file_path)
+            files[file] = file_path
+        elif os.path.isdir(file_path):
+            dir_list = os.listdir(file_path)
+            for item in dir_list:
+                file_list.append(os.path.join(file_path, item))
 
     if len(result_file_list) == 0:
+        print('Соответствий не найдено.')
         return None, None
-
-    print('Найдены следующие соответствия:')
-    result_list = sorted(result_file_list)
-    for i in range(len(result_list)):
-        print(f'{i}. {result_list[i]}')
-    return result_list, files
+    else:
+        print('Найдены следующие соответствия:')
+        result_list = sorted(result_file_list)
+        for i in range(len(result_list)):
+            print(f'{i}. {result_list[i]}\n')
+        return result_list, files
 
 
 def create_file(result_list, files, directory_to_save):
+    '''Create file with search results'''
     file_to_save = os.path.join(directory_to_save, 'files_found.txt')
     with open(file_to_save, 'w', encoding='utf-8') as save:
         for i in range(len(result_list)):
-            save.write(f'{i}. {files[result_list[i]][0]} {files[result_list[i]][1]}\n')
+            save.write(f'{i}. {files[result_list[i]]}\n')
     print('Операция завершена.')
     return files
 
